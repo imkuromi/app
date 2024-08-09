18fdf848c933874e47cb3d0704984c43a3626e7a
imkuromi <156067407+imkuromi@users.noreply.github.com>
2024-08-09 23:03:53 +0700

    Add component
     Add component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Calendar from "./class-component";
+import { Header, Content, Footer } from "./func-components";
 import "./style.css";
 function App() {
   const divStyle = {
@@ -8,9 +10,17 @@ function App() {
   };
   return (
     <>
+    <Header/>
       <div style={divStyle}>Hello React</div>
       <h2>DeveloperThai.com</h2>
       <div className="title">React & React Native</div>
+      <p>
+        <center>
+          <Calendar />
+        </center>
+      </p>
+      <Content/>
+      <Footer/>
     </>
   );
 }
