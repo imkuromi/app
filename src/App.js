b6e3a500e654564665d74e54214a7188ca916165
imkuromi <156067407+imkuromi@users.noreply.github.com>
2024-08-19 11:52:23 +0700

    ส่ง refs-func.js กับ state-func.js
    update app.js
    add ref-func.js
    add sate-class.js
    add sate-func.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Calendar, Button } from "./class-component";
 import { Header, Content, Footer } from "./func-components";
 import { Calculator2 } from "./calculator";
 import { Eventdata2 } from "./event-data";
+import RefsFunc from "./refs-func";
+import Cart from "./sate-class";
+import  Massagebox  from "./sate-func";
 import "./style.css";
 function App() {
   const divStyle = {
@@ -12,23 +15,27 @@ function App() {
   };
   return (
     <>
-    <Header/>
+      <Header />
       <div style={divStyle}>Hello React</div>
       <h2>DeveloperThai.com</h2>
       <div className="title">React & React Native</div>
       <p>
         <center>
           <Calendar />
-          <br/>
-          <Button/>
-          <Calculator2/>
-          <br/>
-          <Eventdata2/>
+          <br />
+          <Button />
+          <Calculator2 />
+          <br />
+          <Eventdata2 />
+          <br />
+          <RefsFunc />
+          <Cart/>
+          <Massagebox/>
         </center>
       </p>
-      
-      <Content/>
-      <Footer/>
+
+      <Content />
+      <Footer />
     </>
   );
 }
